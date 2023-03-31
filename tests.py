04b13cc6e2fb64e91dbb08f255de2d2df3d35f6f
Billy-06 <ndegwah4billy@gmail.com>
2023-03-31 20:42:19 +0800

    added the test form pytest and added a simple bookstore app

--- tests.py
+++ tests.py
@@ -1,2 +1,16 @@
+import pytest
+
+
+class TestClass(object):
+    def test_one(self):
+        x = "this"
+        assert 'h' in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, 'check')
+
+
 def run():
-    print("Hello This is the first test")
+    # Run the tests
+    assert 1 == 1
